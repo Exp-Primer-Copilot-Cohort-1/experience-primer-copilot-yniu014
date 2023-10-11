323d2aad227ab649e3f9e24966b01f1212e1ff33
yniu014 <147244370+yniu014@users.noreply.github.com>
2023-10-11 05:31:11 +0000

    Copilot first commit

---
+++ skills.cpp
@@ -0,0 +1,9 @@
+/**
+ * This function takes in two integers and returns their sum.
+ * 
+ * @param a The first integer to be added.
+ * @param b The second integer to be added.
+ * @return The sum of the two integers.
+ */
+#include <iostream>
+#include <string>
